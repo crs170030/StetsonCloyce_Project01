6a084e819d76ba6d5218c487aae6550ef5c9a6cf
Cloyce Stetson <crs170030@utdallas.edu>
2020-08-26 18:35:35 -0500

    Project Initialization

---
+++ Assets/BalloonRise.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonRise : MonoBehaviour
+{
+    public GameObject balloon;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //balloon.Position += Vector3.up * 5f;
+        balloon.transform.Translate(Time.deltaTime * transform.up * 0.5f);
+    }
+}
